638de9b3cdd1a1300ad3cbe5bacba3610e3abe7a
piyushk6626 <kulkarnipiyush462@gmail.com>
2024-04-04 20:19:27 +0530

    try

--- response.py
+++ response.py
@@ -68,7 +68,7 @@ def user_input(user_question):
     print(response)
     return response
 
-loader = CSVLoader(file_path="static\Database - Sheet1.csv")
+loader = CSVLoader(file_path="static/Database.csv")
 data = loader.load()
 
 
